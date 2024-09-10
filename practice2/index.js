0da6c035bef8f6397c18b4812e5f064eb9dad689
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2024-09-10 14:20:28 +0300

    Practice

---
+++ practice2/index.js
@@ -0,0 +1,20 @@
+import { Logger } from './Logger.js';
+
+const __filename = import.meta.filename;
+
+const __dirname = import.meta.dirname;
+
+const logger = new Logger(__dirname);
+
+logger.info('Асинхронная задача выполнена');
+logger.warn('Файл может быть поврежден асинхронно');
+logger.error('Файл поврежден асинхронно');
+
+logger.infoSync('Синхронная задача выполнена');
+logger.warnSync('Файл может быть поврежден синхронно');
+logger.errorSync('Файл поврежден синхронно');
+
+
+console.log(logger.readLogSync());
+
+console.log(logger.readLog());
